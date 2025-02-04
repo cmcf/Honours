9f7e3f66196159e7683d31b6568dd3d57c4bc8a8
Clare-M <64334337+cmcf@users.noreply.github.com>
2025-02-04 14:15:21 +0000

    Player can take damage
    Melee and range enemy deal damage to player
    Death animation is triggered when player dies

--- Honours/Assets/Scripts/Enemy/EnemyBullet.cs
+++ Honours/Assets/Scripts/Enemy/EnemyBullet.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Damage;
 
 public class EnemyBullet : MonoBehaviour
 {
     float bulletLife = 10f;
+    [SerializeField] float damageAmount = 10;
+
+    bool hitPlayer = false;
     void Start()
     {
         Destroy(gameObject, bulletLife);
@@ -16,6 +20,31 @@ public class EnemyBullet : MonoBehaviour
         if (collision.CompareTag("Wall"))
         {
             Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("Player") && !hitPlayer)
+        {
+            // Deals damage to player if hit
+            hitPlayer = true;
+            Debug.Log("Hit");
+            DealDamage(collision.transform);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        hitPlayer = false;
+    }
+
+    void DealDamage(Transform target)
+    {
+        // Check if the target has a damageable component
+        IDamageable damageable = target.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            // Deal damage to the player
+            damageable.Damage(damageAmount);
         }
     }
 }
